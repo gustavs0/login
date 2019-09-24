a08138276aee2c156e3a0157b51e45e9a5dc3e2b
Gustavo <gustavo_castro@estudante.sc.senai.br>
2019-09-24 20:57:43 -0300

    Formulario de login
    closes #1

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
 <!--para acessar o projeto http://localhost/login/ -->
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -9,31 +10,57 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Sistema de Login</title>
 </head>
-<body class ="bg-dark">
+
+<body class="bg-dark">
     <!-- fundo escuro -->
     <main class="container mt-4">
-      <section class= "row">
-          <div class="col-lg-4 offset-lg-4" id="alerta">
-              <div class="alert alert-success text-center">
-                  <strong>
-                      Alo Ha tchurmaaaaaaaaaaaa
-                  </strong>
-              </div>
-          </div>
-      </section>
-      <!-- formulário de login -->
-      <section class="row">
-          <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
-              <h2 class="text-center mt-">
-                  Entrar no Sistema 
-              </h2>
-              <form action="#" method="post" class="p-2"id="formLogin">
-                  <div class="form-group">
-                      <input type="text" name="" id="">
-                  </div>
-              </form>
-          </div>
-      </section>
+        <section class="row">
+            <div class="col-lg-4 offset-lg-4" id="alerta">
+                <div class="alert alert-success text-center">
+                    <strong>
+                        Alo Ha tchurmaaaaaaaaaaaa
+                    </strong>
+                </div>
+            </div>
+        </section>
+        <!-- formulário de login -->
+        <section class="row">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
+                <h2 class="text-center mt-">
+                    Entrar no Sistema
+                </h2>
+                <form action="#" method="post" class="p-2" id="formLogin">
+                    <div class="form-group">
+                        <input type="text" name="nomeUsuario" id="nomeUsuario" placeholder="Nome de Usuário" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="senhaUsuario" id="senhaUsuario" placeholder="Senha" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" name="lembrar" id="lembrar" class="custom-control-input">
+                            <label for="lembrar" class="custom-control-label">
+                                Lembrar de mim.
+                            </label>
+                            <a href="#" class="float-right" id="btnEsqueci">
+                                Esqueci a senha
+                            </a>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="::Entrar::" name="btnEntrar" id="btnEntrar" class="btn btn-primary btn-block">
+                    </div>
+                    <div class="form-group">
+                        <p>
+                            Novo usuário?
+                                <a href="#" id="btnRegistrar">
+                                    Registre-se aqui.
+                                </a>
+                        </p>
+                    </div>
+                </form>
+            </div>
+        </section>
     </main>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -41,4 +68,5 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
+
 </html>
