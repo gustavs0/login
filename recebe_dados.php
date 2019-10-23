71eb004df6a4139d43079c13c393f1d3ad183a19
Gustavo <gustavo.da.costa12@gmail.com>
2019-10-22 22:34:47 -0200

    close aula09

--- recebe_dados.php
+++ recebe_dados.php
@@ -86,6 +86,15 @@ if (isset($_POST['action'])) {
         $busca = $sql->fetch();
         if ($busca != null) {
             $_SESSION['nomeDoUsuario'] = $nomeUsuario;
+            if(!empty($_POST['lembrar'])){
+                //se lembrar nao esticver vazio! ou seja, a pessoa quer ser lembrada
+                setcookie("nomeDoUsuario", $nomeUsuario, time()+(60*60*24*30));
+                setcookie("senhaDoUsuario", $senhaUsuario, time()+(+60*60*24*30));
+            }else{
+                //a pessoa nao quer ser lembrada
+                setcookie("nomeDoUsuario", "");
+                setcookie("senhaDoUsuario", "");
+            }
             echo "ok";
         } else {
             echo "<p class='text-danger'>";
